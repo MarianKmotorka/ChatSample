80e9824c4ebccc1f21c61011a7322596c72995b0
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-07-03 20:26:08 +0200

    Migrations folder path 2

--- ChatSampleApi/ChatSampleApi/Persistence/Migrations/20200703182432_Initial.cs
+++ ChatSampleApi/ChatSampleApi/Persistence/Migrations/20200703182432_Initial.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace ChatSampleApi.Persistence
+namespace ChatSampleApi.Persistence.Migrations
 {
     public partial class Initial : Migration
     {
--- ChatSampleApi/ChatSampleApi/Persistence/Migrations/DatabaseContextModelSnapshot.cs
+++ ChatSampleApi/ChatSampleApi/Persistence/Migrations/DatabaseContextModelSnapshot.cs
@@ -4,16 +4,14 @@ using ChatSampleApi.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace ChatSampleApi.Persistence
+namespace ChatSampleApi.Persistence.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    [Migration("20200703181846_Initial")]
-    partial class Initial
+    partial class DatabaseContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
