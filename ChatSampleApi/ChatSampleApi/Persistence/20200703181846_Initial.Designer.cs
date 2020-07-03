2195e1e47d24e3e5bbb2ddf4e73e43068c9c8c85
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-07-03 20:20:15 +0200

    Migrations Folder PAth

--- ChatSampleApi/ChatSampleApi/Persistence/20200703181846_Initial.Designer.cs
+++ ChatSampleApi/ChatSampleApi/Persistence/20200703181846_Initial.Designer.cs
@@ -4,14 +4,16 @@ using ChatSampleApi.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace ChatSampleApi.Migrations
+namespace ChatSampleApi.Persistence
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200703181846_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ChatSampleApi/ChatSampleApi/Persistence/20200703181846_Initial.cs
+++ ChatSampleApi/ChatSampleApi/Persistence/20200703181846_Initial.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace ChatSampleApi.Migrations
+namespace ChatSampleApi.Persistence
 {
     public partial class Initial : Migration
     {
