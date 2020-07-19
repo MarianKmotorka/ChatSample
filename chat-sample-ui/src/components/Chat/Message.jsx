d459b3990d77108c923b5a0c54dd832b10ad20b0
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-07-19 13:42:17 +0200

    Memoize Message component

--- chat-sample-ui/src/components/Chat/Message.jsx
+++ chat-sample-ui/src/components/Chat/Message.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { memo } from 'react'
 import moment from 'moment'
 import PropTypes from 'prop-types'
 import { MessageWrapper, MessageInfo, MessageDate, Text } from './styled/Chat.styled'
 
-const Message = ({ message, forwardRef }) => (
+const Message = memo(({ message, forwardRef }) => (
   <MessageWrapper isMyMessage={message.isMyMessage} ref={forwardRef}>
     <MessageInfo>
       <img referrerPolicy='no-referrer' src={message.senderPicture} alt='' />
@@ -12,7 +12,7 @@ const Message = ({ message, forwardRef }) => (
     </MessageInfo>
     <Text className='wordwrap'>{message.text}</Text>
   </MessageWrapper>
-)
+))
 
 Message.propTypes = {
   message: PropTypes.shape({
