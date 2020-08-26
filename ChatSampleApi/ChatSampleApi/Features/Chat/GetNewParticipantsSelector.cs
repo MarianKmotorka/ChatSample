4343d9d701ac818f6594a17d268183165c88f30a
Marian Kmotorka <marian.kmotorka@quadrotech-it.com>
2020-08-26 22:15:03 +0200

    NuGet updates

--- ChatSampleApi/ChatSampleApi/Features/Chat/GetNewParticipantsSelector.cs
+++ ChatSampleApi/ChatSampleApi/Features/Chat/GetNewParticipantsSelector.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ChatSampleApi.Exceptions;
 using ChatSampleApi.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
